0d4c4edacea7a6aa3f179e44e404e9905295c172
mischa <mischa.mickler@web.de>
2019-11-08 11:20:19 +0100

    try to change an icon

--- dist/index.js
+++ dist/index.js
@@ -8527,8 +8527,19 @@ function (_PureComponent) {
         // href="https://getstream.io/chat/"
         target: "_blank",
         rel: "noopener noreferrer",
-        className: "str-chat__square-button str-chat__header-livestream-right-button--info"
+        className: "str-chat__square-button str-chat__header-livestream-right-button--apple"
       }, React__default.createElement("svg", {
+        width: "4",
+        height: "14",
+        xmlns: "http://www.w3.org/2000/svg"
+      }, React__default.createElement("path", {
+        d: "M3 13h1v.5H0V13h1V5.5H0V5h3v8zM1.994 3.516A1.507 1.507 0 1 1 1.995.502a1.507 1.507 0 0 1-.001 3.014z",
+        fillRule: "evenodd"
+      }))), React__default.createElement("a", {
+        target: "_blank",
+        rel: "noopener noreferrer",
+        className: "str-chat__square-button str-chat__header-livestream-right-button--info"
+      }, " TEST", React__default.createElement("svg", {
         width: "4",
         height: "14",
         xmlns: "http://www.w3.org/2000/svg"
--- src/components/ChannelHeader.js
+++ src/components/ChannelHeader.js
@@ -61,8 +61,23 @@ class ChannelHeader extends PureComponent {
               // href="https://getstream.io/chat/"
               target="_blank"
               rel="noopener noreferrer"
+              className="str-chat__square-button str-chat__header-livestream-right-button--apple"
+            > 
+              <svg width="4" height="14" xmlns="http://www.w3.org/2000/svg">
+                <path
+                  d="M3 13h1v.5H0V13h1V5.5H0V5h3v8zM1.994 3.516A1.507 1.507 0 1 1 1.995.502a1.507 1.507 0 0 1-.001 3.014z"
+                  fillRule="evenodd"
+                />
+              </svg>
+            </a>
+
+            {/* copied from above   */}
+            <a
+              
+              target="_blank"
+              rel="noopener noreferrer"
               className="str-chat__square-button str-chat__header-livestream-right-button--info"
-            >
+            > TEST
               <svg width="4" height="14" xmlns="http://www.w3.org/2000/svg">
                 <path
                   d="M3 13h1v.5H0V13h1V5.5H0V5h3v8zM1.994 3.516A1.507 1.507 0 1 1 1.995.502a1.507 1.507 0 0 1-.001 3.014z"
