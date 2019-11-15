6ba9c7be1a1fbca103e662b838ee22dd7a528f49
mischa <mischa.mickler@web.de>
2019-11-15 15:01:07 +0100

    try to fix an error (this.setState is not a function)

--- dist/index.js
+++ dist/index.js
@@ -5164,7 +5164,7 @@ function (_PureComponent) {
       })));
     });
 
-    window.addEventListener(languageChangedEventName, _this.onLanguageChanged);
+    window.addEventListener(languageChangedEventName, _this.onLanguageChanged());
     return _this;
   }
 
@@ -5186,7 +5186,7 @@ function (_PureComponent) {
         document.removeEventListener('touchend', this._closeDetailedReactions);
       }
 
-      document.removeEventListener(languageChangedEventName, this.onLanguageChanged);
+      document.removeEventListener(languageChangedEventName, this.onLanguageChanged());
     }
   }, {
     key: "isMine",
