dccc24ab391a2c3cdb2c1f2d5a37507735795a61
mischa <mischa.mickler@web.de>
2019-11-15 11:41:38 +0100

    remove DPL-CHat:

--- src/components/MessageSimple.js
+++ src/components/MessageSimple.js
@@ -28,7 +28,6 @@ export class MessageSimple extends PureComponent {
     super();
     window.addEventListener(languageChangedEventName, () => {
       this.onLanguageChanged();
-      console.log(this);
     });
   }
 
